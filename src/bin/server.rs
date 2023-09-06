c0f4ad03ee4e27ae8036d593ed1af5f30e1ae2bb
Sparrow1029 <19390913+Sparrow1029@users.noreply.github.com>
2023-09-06 17:08:38 -0600

    Modified server to use tokio + async

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,122 +1,162 @@
-use anyhow::Result;
-use std::cell::RefCell;
-use std::collections::{HashMap, VecDeque};
-use std::io::{ErrorKind, Read, Write};
-use std::net::{SocketAddr, TcpListener, TcpStream};
+use anyhow::{anyhow, Result};
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+use std::io::ErrorKind;
+use std::net::SocketAddr;
+use tokio::io::AsyncWriteExt;
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::mpsc::{self, Receiver, Sender};
+use tokio::sync::Mutex;
+use tokio::task;
 
-use simple_chat::{sleep, Message, BUFSIZE};
+use simple_chat::{Message, BUFSIZE};
 
-#[derive(Debug)]
-struct Server {
-    clients: HashMap<SocketAddr, TcpStream>,
-    listener: TcpListener,
-    msg_q: RefCell<VecDeque<Message>>,
+lazy_static! {
+    static ref PEERS: PeerMap = Mutex::new(HashMap::new());
 }
 
-impl Server {
-    fn create(addr: &str) -> Result<Self> {
-        let socket_addr: SocketAddr = addr.parse()?;
-        let listener = TcpListener::bind(socket_addr)?;
-        listener.set_nonblocking(true)?;
-        let clients = HashMap::new();
-        let msg_q = RefCell::new(VecDeque::new());
+type PeerMap = Mutex<HashMap<SocketAddr, OwnedWriteHalf>>;
 
-        Ok(Server {
-            listener,
-            clients,
-            msg_q,
-        })
-    }
-
-    fn run(&mut self) -> Result<()> {
-        loop {
-            match self.listener.accept() {
-                Ok((stream, socket_addr)) => {
-                    _ = self.handle_connection(socket_addr, stream);
-                }
-                _ => {
-                    sleep(100);
-                }
+async fn run_server(addr: &str) -> Result<()> {
+    let addr: SocketAddr = addr.parse()?;
+    let listener = TcpListener::bind(addr).await?;
+    let (tx, rx) = mpsc::channel(32);
+    // start broadcast task
+    task::spawn(broadcast(rx));
+    loop {
+        match listener.accept().await {
+            Ok((stream, addr)) => {
+                println!("New client: {addr}");
+                task::spawn(handle_connection(stream, addr, tx.clone()));
             }
-            self.recv_messages()?;
-            let keys_to_drop = self.send_messages()?;
-            self.drop_connections(&keys_to_drop);
-        }
-    }
-
-    fn handle_connection(&mut self, addr: SocketAddr, stream: TcpStream) -> Result<()> {
-        stream.set_nonblocking(true)?;
-        let client_addr = stream.peer_addr()?;
-        println!("New connection from {client_addr}");
-        self.clients.insert(addr, stream);
-        Ok(())
-    }
-
-    fn drop_connections(&mut self, keys_to_drop: &[SocketAddr]) {
-        for key in keys_to_drop {
-            println!("Dropping connection to {key}");
-            self.clients.remove(key);
-        }
-    }
-
-    fn recv_messages(&mut self) -> Result<()> {
-        let mut dropped_connections = vec![];
-        for (addr, stream) in self.clients.iter_mut() {
-            let mut buf: [u8; BUFSIZE] = [0; BUFSIZE];
-            if let Ok(bytes_read) = stream.read(&mut buf) {
-                if bytes_read > 0 {
-                    let msg = Message::try_from(&buf[..])?;
-                    self.msg_q.borrow_mut().push_back(msg);
-                    stream.flush()?;
-                } else if bytes_read == 0 {
-                    eprintln!("Connection to {addr} closed.");
-                    dropped_connections.push(*addr)
-                }
+            Err(e) => {
+                eprintln!("couldn't get client {:?}", e);
+                return Err(anyhow!("Couldn't get client connection {:?}", e));
             }
         }
-        self.drop_connections(&dropped_connections);
-        Ok(())
     }
+}
 
-    fn send_messages(&mut self) -> Result<Vec<SocketAddr>> {
-        let mut dropped_connections = vec![];
+async fn handle_connection(stream: TcpStream, addr: SocketAddr, tx: Sender<Message>) -> Result<()> {
+    let (r, mut w) = stream.into_split();
+    if let Some(msg) = get_msg_from_stream(&r).await {
+        let mut peermap = PEERS.lock().await;
+        peermap.insert(addr, w);
+        drop(peermap);
+        tx.send(msg).await?;
+        task::spawn(recv_messages(tx, r));
+    } else {
+        // disconnect client/shutdown stream
+        w.shutdown().await?;
+    }
+    Ok(())
+}
 
-        // Have to access self.msg_q through RefCell (interior mutability)
-        // to allow borrowing fields on self mutably more than once.
-        // It should be safe because we are mutating two _separate_ fields of `self`
-        // println!("in send_messages: {}", self.msg_q.borrow().len());
-        while let Some(msg) = self.msg_q.borrow_mut().pop_front() {
-            for (addr, stream) in self.clients.iter_mut() {
-                let encoded = msg.serialize()?;
-                match stream.write(&encoded) {
-                    Ok(_bytes_written) => {
-                        stream.flush()?;
-                        dbg!(_bytes_written, addr);
-                    }
-                    Err(e) if e.kind() == ErrorKind::BrokenPipe => {
-                        println!("Broken pipe. Dropping connection to peer at {addr}");
-                        dropped_connections.push(*addr);
-                    }
+async fn get_msg_from_stream(stream: &OwnedReadHalf) -> Option<Message> {
+    let mut buf = [0u8; BUFSIZE];
+    match stream.try_read(&mut buf) {
+        Ok(bytes_read) => {
+            if bytes_read == 0 {
+                eprintln!("Client disconnected.");
+                None
+            } else {
+                Some(Message::try_from(&buf[..]).expect("Error serializing msg from stream"))
+            }
+        }
+        Err(ref e) if e.kind() == ErrorKind::WouldBlock => None,
+        Err(e) => {
+            eprintln!("Error getting message from stream {}: {e}", e.kind());
+            None
+        }
+    }
+}
+
+async fn broadcast(rx: Receiver<Message>) -> Result<()> {
+    let mut rx = rx;
+    loop {
+        if let Ok(msg) = rx.try_recv() {
+            if msg.content.len() == 0usize {
+                continue;
+            }
+            let encoded = msg.serialize()?;
+            let mut peermap = PEERS.lock().await;
+            // eprintln!(
+            //     "Sending msg to all users {:?}",
+            //     std::str::from_utf8(&msg.content)?
+            // );
+            for (addr, stream) in peermap.iter_mut() {
+                stream.writable().await?;
+                match stream.try_write(&encoded) {
+                    Ok(_) => {}
                     Err(e) => {
-                        println!("Error '{}: {e}' trying to write to client stream. Adding to dropped connections.", e.kind());
-                        dropped_connections.push(*addr);
+                        eprintln!("Error writing to {addr}. {}: {e}", e.kind());
                     }
                 }
             }
+            drop(peermap);
         }
-        Ok(dropped_connections)
     }
 }
 
-fn main() -> Result<()> {
+async fn recv_messages(tx: Sender<Message>, stream: OwnedReadHalf) -> Result<()> {
+    loop {
+        let mut buf = [0u8; BUFSIZE];
+        stream.readable().await?;
+        match stream.try_read(&mut buf) {
+            Ok(bytes_read) => {
+                if bytes_read == 0 {
+                    // client disconnected
+                    eprintln!(
+                        "No bytes read from ({:?}). Dropping connection.",
+                        stream.peer_addr()
+                    );
+                    drop_peer_connection(stream, tx).await?;
+                    break;
+                }
+                let msg = Message::try_from(&buf[..])?;
+                tx.send(msg).await?;
+            }
+            Err(e) if e.kind() == ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) if e.kind() == ErrorKind::ConnectionReset => {
+                drop_peer_connection(stream, tx).await?;
+                break;
+            }
+            Err(e) => {
+                eprintln!(
+                    "Error receiving messages from client ({:?}). {}: {e}",
+                    stream.peer_addr(),
+                    e.kind()
+                );
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn drop_peer_connection(stream: OwnedReadHalf, tx: Sender<Message>) -> Result<()> {
+    let addr = stream.peer_addr()?;
+    let mut peermap = PEERS.lock().await;
+    peermap.remove(&addr);
+    drop(stream);
+    tx.send(Message {
+        username: "user".to_string(),
+        content: b"has left the chat.".to_vec(),
+    })
+    .await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let usage = "Usage: server <addr:port>";
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
         println!("{usage}");
         std::process::exit(1);
     }
-    let mut server = Server::create(&args[1])?;
-    dbg!("Created server {:?}", &server);
-    server.run()?;
+    run_server(&args[1]).await?;
     Ok(())
 }
